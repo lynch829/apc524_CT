d93e1a539a301db62adb030a636103011ef3b6da
erhulao <liqi1026@gmail.com>
2014-12-13 15:49:21 -0500

    Started adding interpolator, first commit, test for creating a Numerical curve from AnaCurve

--- test/test_anaimage.cpp
+++ test/test_anaimage.cpp
@@ -35,6 +35,9 @@ int main(int argc, char* argv[]){
 //  creates a nonsymmetric gaussian on 20 x 20 region.
     Image* gauss  = new AnaSurface( gauss_2D, 10, 10);
 
+// creates a numerical line using gauss_1D.
+    Image* gauss1 = new AnaCurve( gauss_1D,10);
+    //Image* NumGauss = new NumCurve(100,gauss1);
 // defines trapezoid integration rule.
     LineIntegral* l=new Trapezoid();
 
@@ -42,7 +45,7 @@ int main(int argc, char* argv[]){
 
     switch(argc)
     {
-        case 1 : {gauss->Print(); delete gauss; return 0;}
+        case 1 : {gauss1->Print(); delete gauss1; return 0;}
         case 2 : {
 		 Surface* ptr = (Surface*)gauss; //Image has no GetProjection, must downcast.
                  NumCurve a; a = ptr->GetProjection(l,atof(argv[1]),0.01);
