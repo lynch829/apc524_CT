8ef1ace95655436be1f7bfbbc7cbcf249aaef343
erhulao <liqi1026@gmail.com>
2014-12-15 21:08:48 -0500

    test one NumSurface constructor, output gives expected results.

--- test/test_anaimage.cpp
+++ test/test_anaimage.cpp
@@ -2,6 +2,7 @@
 #include "AnaImage.h"
 #include "NumCurve.h"
 #include "NumSurface.h"
+#include "Surface.h"
 #include "Trapezoid.h"
 #include "MCIntegrator.h"
 #include <math.h>
@@ -37,10 +38,12 @@ int main(int argc, char* argv[]){
     Image* gauss  = new AnaSurface( gauss_2D, 10, 10);
 
 // creates a numerical line using gauss_1D.
-    Curve* gauss1 = new AnaCurve( gauss_1D,10);
-    Image* num_gauss1 = new NumCurve(100,*gauss1);
+//    Curve* gauss1 = new AnaCurve( gauss_1D,10);
+//    Image* num_gauss1 = new NumCurve(100,*gauss1);
     
-    
+// creates a numerical surface using gass_2D.
+    Surface* gauss2 = new AnaSurface( gauss_2D,10,10);
+    NumSurface* num_gauss2 = new NumSurface(100,100,*gauss2);   
 // defines trapezoid integration rule.
     LineIntegral* l=new Trapezoid();
 
@@ -48,7 +51,8 @@ int main(int argc, char* argv[]){
 
     switch(argc)
     {
-        case 1 : {num_gauss1->Print(); delete gauss1; delete num_gauss1;return 0;}
+       case 1 : {num_gauss2->Print(); delete gauss2; delete num_gauss2;return 0;}
+  //     case 1 : {num_gauss1->Print(); delete gauss1; delete num_gauss1;return 0;}
         case 2 : {
 		 Surface* ptr = (Surface*)gauss; //Image has no GetProjection, must downcast.
                  NumCurve a; a = ptr->GetProjection(l,atof(argv[1]),0.01);
