c0b3068363c25d10b2dfc38154088cdcdfcd426f
suerfu <suerfu@princeton.edu>
2015-01-03 19:03:44 -0500

    * added prototype for FilteredBackProjection3D.

--- src/FilteredBackProjection.cpp
+++ src/FilteredBackProjection.cpp
@@ -6,6 +6,7 @@
 
 NumSurface* FilteredBackProjection( ImageArray& array, int Nres, double (*kernal)(int,double))
 {
+    // Nres is the resolution for the reconstructed surface.
     const double range = array.GetRange();
 
     array.ConvolveWithKernal(kernal);	// filtered
@@ -25,41 +26,7 @@ NumSurface* FilteredBackProjection( ImageArray& array, int Nres, double (*kernal
     return rec;
 }
 
-NumSurface* FilteredSymmetricBackProjection( double* angle, Curve* curve, int size, int Nres)
+NumVolume* FilteredBackProjection3D( ImageArray& array, int Nres,double (*kernal)(int,double))
 {
-    NumCurve* curves = (NumCurve*) curve;
-    double range = curves[0].GetRange();
-    
-//---------------convolute with ramp filter--------------------------------------
 
-        double tau = 2*range/(curves[0].GetSize()-1);
-        NumCurve conv = curves[0];
-        for(int j=0;j<curves[0].GetSize();j++){
-            conv[j] = 0;
-            for(int k=j-curves[0].GetSize()+1;k<j+1;k++)
-                conv[j] +=tau*Hamming(k,tau)*(curves[0])[j-k];
-        }
-        curves[0]=conv;
-
-    double **z = new double*[Nres];
-    for(int i=0;i<Nres;i++){
-        z[i] = new double[Nres];
-        for(int j=0;j<Nres;j++) z[i][j] = 0;
-    }
-
-    for(int ll=0; ll<size; ll++){
-        double angl = 0 + ll*pi/(size-1);
-        for( int i=0; i<Nres; i++)
-            for( int j=0; j<Nres; j++){
-                double x = -range + i*2*range/(Nres-1);
-                double y = -range + j*2*range/(Nres-1);
-                double t = x*cos(angl) - y*sin(angl);
-                z[i][j] += (curves)[0](t,0)/size;
-            }
-    }
-    NumSurface* recon = new NumSurface(Nres,range,Nres,range,z);
-    for(int i=0;i<Nres;i++) delete [] z[i];
-    delete [] z;
-    return recon;
 }
-
